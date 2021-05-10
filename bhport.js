637a5f0621630687e1c7b60fd224a2605dd19c35
Hanna <Bethany.Hanna@autofinance.com>
2021-05-09 21:51:30 -0400

    final revision for Gold Badge

--- bhport.js
+++
@@ -1,5 +0,0 @@
-//want something fun/funky but still professional
-//want to have dark mode ability with something fun/cute - button/animation/both similar to github's
-//want to wireframe, first by hand, then by using free figma or adobe, maybe both to see how they compare
-//need to research: fonts, animations, colors/backgrounds light vs dark, bootstrap stylings, contact me style on formspree
-//need to update LinkedIn, resume, else?
